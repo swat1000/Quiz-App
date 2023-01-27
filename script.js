0e2e7fe7ca98d9d1ef6eb1da555ff937a0fb528d
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-01-27 16:29:24 +0530

    count function updated

--- script.js
+++ script.js
@@ -38,15 +38,19 @@ const quizData = [
 const quiz= document.getElementById('quiz')
 const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
+const questionCountEl = document.getElementById("question-count");
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
 const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
 
+var currentQuiz = 0;
+let score = 0;
 
-let currentQuiz = 0
-let score = 0
+
+
+
 
 loadQuiz()
 
@@ -82,19 +86,20 @@ submitBtn.addEventListener('click', () => {
     const answer = getSelected()
     if(answer) {
        if(answer === quizData[currentQuiz].correct) {
-           score++
+           score++;
        }
 
-       currentQuiz++
+          currentQuiz++;
 
        if(currentQuiz < quizData.length) {
            loadQuiz()
        } else {
            quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+           <h2>You answered ${currentQuiz}/${quizData.length} questions correctly</h2>
 
            <button onclick="location.reload()">Reload</button>
            `
        }
     }
+    questionCountEl.textContent = `Question Attempted: ${currentQuiz + 1} of /${quizData.length} questions`;
 })
