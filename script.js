fbeb7b925f18fce22dc4daf848707898ea5d0319
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-01-27 15:00:52 +0530

    Qustions added in JS

---
+++ script.js
@@ -0,0 +1,37 @@
+const quizData = [
+    {
+        question: "Which of the following is correct about JavaScript?",
+        a: "JavaScript is an Object-Based language",
+        b: "JavaScript is Assembly-language",
+        c: "JavaScript is an Object-Oriented language",
+        d: "JavaScript is a High-level language",
+        correct: "a",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+
+
+];
+
